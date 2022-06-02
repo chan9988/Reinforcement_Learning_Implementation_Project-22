c114fd3e38efdb00dafe09a184765d8faafdd3c1
chan9988 <kai9988ckc.cs07@nctu.edu.tw>
2022-06-02 17:26:53 +0800

    env init

--- code/Lunar_Lander.py
+++ code/Lunar_Lander.py
@@ -9,15 +9,30 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions import Categorical
 import torch.optim.lr_scheduler as Scheduler
+import itertools
 
 
 def train():
-    return
 
+    ewma_rewaord=0
+
+    for i_episode in itertools.count(1):
+        state = env.reset()
+    
+        ep_reward=0
+        max_episode_len = 10000
+        for t in range(max_episode_len):
+            action = np.random.choice([0,1,2,3])
+            state, reward, done, _ = env.step(action)
+            ep_reward += reward
+            if done:
+                break
+        
+        print(ep_reward)
 
 if __name__ == '__main__':
     random_seed = 22
-    env = gym.make('LunarLanderContinuous-v2')
+    env = gym.make('LunarLander-v2')
     env.reset(seed=random_seed)  
     torch.manual_seed(random_seed) 
     train()

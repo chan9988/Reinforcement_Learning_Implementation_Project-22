83cccee8ce9bcc619fce288dbd079710560f20e5
chan9988 <kai9988ckc.cs07@nctu.edu.tw>
2022-06-02 14:44:02 +0800

    sample_test

--- code/sample_mdp.py
+++ code/sample_mdp.py
@@ -1,21 +1,56 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class sample_mdp:
     def __init__(self):
-        self.state=1
+        self.state=0
     
     def take_action(self,move):
         reward=0
         terminal=False
-        if self.state==1:
+        if self.state==0:
             if move == 0:
                 reward=0.122
-                self.state = np.random.choice([1,2],p=[0.66,0.34])
+                self.state = np.random.choice([0,1],p=[0.66,0.34])
             elif move == 1:
                 reward=0.033
-                self.state = np.random.choice([1,2],p=[0.99,0.01])
+                self.state = np.random.choice([0,1],p=[0.99,0.01])
 
-        elif self.stat==2:
-            reward=0
+        if self.state==1:
             terminal=True
+
         return self.state,reward,terminal
+
+
+
+def train():
+    Q_a=0
+    Q_b=0
+    episode=1000
+    alpha=0.1
+    gamma=0.98
+    for _ in range(episode):
+        env=sample_mdp()
+        trajectory=[]
+        trajectory.append(env.state)
+        ter=False
+        while not ter:
+            action = np.random.choice([0,1],p=[0.5,0.5])
+            state,reward,ter=env.take_action(action)
+            
+            if ter:
+                if action == 0:
+                    Q_a = Q_a+alpha * (reward - Q_a)
+                elif action == 1:
+                    Q_b = Q_b+alpha * (reward - Q_b)
+            else:
+                if action == 0:
+                    Q_a = Q_a+alpha * (reward + gamma * Q_a - Q_a)
+                elif action == 1:
+                    Q_b = Q_b+alpha * (reward + gamma * Q_b - Q_b)
+
+            trajectory.append(state)
+        
+        print(Q_a,Q_b)
+
+train()

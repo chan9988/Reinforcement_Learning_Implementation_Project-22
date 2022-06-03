1c6615313070fcd23312986df0a26e0b35c66caa
chan9988 <kai9988ckc.cs07@nctu.edu.tw>
2022-06-03 15:49:27 +0800

    6_3

--- code/Lunar_Lander.py
+++ code/Lunar_Lander.py
@@ -11,17 +11,15 @@ from torch.distributions import Categorical
 import torch.optim.lr_scheduler as Scheduler
 import itertools
 
-
-
 def select_action_Boltzmann_softmax(beta,action,c):
-    p=[]
-    for act in action:
-        p.append( math.exp(beta * act - c) )
-    sum_p = sum(p)
-    for i in range(len(p)):
-        p[i] = p[i]/sum_p
+    for i in range(len(action)):
+        action[i] = math.exp(beta * action[i] - c)
 
-    return np.random.choice(range(0,len(p)),p=p)
+    sum_p = sum(action)
+    for i in range(len(action)):
+        action[i] = action[i]/sum_p
+    
+    return action
 
 def mm_omega(omega,action,c):
     m=[]
@@ -43,47 +41,113 @@ def mm_calculate_beta(omega,action,c):
     return sol.root
 
 def select_action_Mellowmax(omega,action,c):
-    beta=mm_calculate_beta(omega,action,c)
+    beta = mm_calculate_beta(omega,action,c)
     return select_action_Boltzmann_softmax(beta,action,c)
-
 
 class Policy(nn.Module):
     def __init__(self):
         super(Policy, self).__init__()
 
-    def forward(self, state):
-        return
+        self.observation_dim = env.observation_space.shape[0]
+        self.action_dim = env.action_space.n 
+        self.hidden_size = 32
+
+
+        self.layer1 = nn.Sequential(nn.Linear(self.observation_dim, self.hidden_size), nn.ReLU(True))
+        self.layer2 = nn.Sequential(nn.Linear(self.hidden_size, self.hidden_size), nn.ReLU(True))
+        self.layer3 = nn.Sequential(nn.Linear(self.hidden_size, self.action_dim))
+
+        self.saved_actions = []
+        self.rewards = []  
+
+    def forward(self, state):       
+        x = self.layer1(state)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = F.softmax(x,dim=1)
+        #x = select_action_Boltzmann_softmax(5,x,max(x).item())
+
+        return x
 
     def select_action(self, state):
-        return
+        state=torch.tensor(state).float().unsqueeze(0)
+        probs = self(state)
+        m = Categorical(probs)
+        action = m.sample()
+        self.saved_actions.append(m.log_prob(action))
+
+        return action.item()
 
     def calculate_loss(self, gamma=0.99):
-        return 
+        R = 0
+        saved_actions = self.saved_actions
+        policy_losses = [] 
+        returns = []
+
+        for r in self.rewards[::-1]:
+            R = r*0.001 + gamma * R
+            returns.insert(0,R) 
+                  
+        for L, R in zip(saved_actions, returns):
+            policy_losses.append(- L * R)        
+        
+        loss=sum(policy_losses)
+        loss.backward()                 
+        
+        return loss
+
+    def clear_memory(self):
+        del self.rewards[:]
+        del self.saved_actions[:]
 
 def train():
 
+    # Instantiate the policy model and the optimizer
     model = Policy()
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    
+    # Learning rate scheduler (optional)
+    scheduler = Scheduler.StepLR(optimizer, step_size=100, gamma=0.9)
+
     omega = 5
-    # for test
+    #for test
     #action=[6,9.1,9,6]
     #print(select_action_Boltzmann_softmax(omega,action,max(action)))
     #print(select_action_Mellowmax(omega,action,max(action)))
 
-    ewma_rewaord=0
+    ewma_reward=0
+    loss=0
 
     for i_episode in itertools.count(1):
         state = env.reset()
     
         ep_reward=0
-        max_episode_len = 10000
+        max_episode_len = 1000
         for t in range(max_episode_len):
-            action = np.random.choice([0,1,2,3])
+            action = model.select_action(state)
             state, reward, done, _ = env.step(action)
+            model.rewards.append(reward)
             ep_reward += reward
             if done:
                 break
         
-        print(ep_reward)
+        optimizer.zero_grad()
+        loss=model.calculate_loss()
+        optimizer.step()
+        model.clear_memory()
+
+        
+        ewma_reward = 0.05 * ep_reward + (1 - 0.05) * ewma_reward
+        
+        #print(loss)
+        if i_episode%10 == 0:
+            print('Episode {}\tlength: {}\treward: {}\t ewma reward: {}\t loss: {}'.format(i_episode, t, ep_reward, ewma_reward, loss))
+            #print(loss)
+        if ewma_reward > env.spec.reward_threshold:
+            #torch.save(model.state_dict(), './preTrained/LunarLander_{}.pth'.format(lr))
+            print("Solved! Running reward is now {} and "
+                  "the last episode runs to {} time steps!".format(ewma_reward, t))
+            break
 
 
 if __name__ == '__main__':
